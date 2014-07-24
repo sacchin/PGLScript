a1a86b7dbc5a9b2d40ffef523c652462121ed082
satonaka yusuke <saonaka.yusuke@isid.co.jp>
2014-07-24 09:10:50 +0900

    first comitte

---
+++ pglgetter.rb
@@ -0,0 +1,105 @@
+#! ruby -Ku
+require 'net/http'
+require "uri"
+require 'json'
+require 'mysql2'
+
+PGLURL = "http://3ds.pokemon-gl.com/frontendApi/gbu/getSeasonPokemonDetail";
+
+def showArray(hashObj)
+hashObj.each{|key, value|
+  if(value.kind_of?(Hash)) then
+	showHash(value)
+  elsif (value.kind_of?(Array)) then
+  puts("array - ", value)
+	value.each{|item| puts("item - ", item)}
+  else 
+puts  value.class.name
+	puts(key + " - ", value)
+  end
+}
+end
+
+def showHash(hashObj)
+hashObj.each{|key, value|
+  if(value.kind_of?(Hash)) then
+	showHash(value)
+  elsif (value.kind_of?(Array)) then
+  puts("array - ", value)
+	value.each{|item| puts("item - ", item)}
+  else 
+puts  value.class.name
+	puts(key + " - ", value)
+  end
+}
+end
+
+def postPGL(pockemonNo)
+uri = URI.parse(PGLURL)
+
+response = Net::HTTP.start(uri.host, uri.port){|http|
+  request = Net::HTTP::Post.new(uri.path)
+  request["referer"] = "http://3ds.pokemon-gl.com/battle/"
+request.set_form_data({
+'languageId'=>'1', 
+'seasonId'=>'4',
+'battleType'=>'0',
+'timezone'=>'JST',
+'pokemonId'=>pockemonNo,
+'displayNumberWaza'=>'10',
+'displayNumberTokusei'=>'3',
+'displayNumberSeikaku'=>'10',
+'displayNumberItem'=>'10',
+'displayNumberLevel'=>'10',
+'displayNumberPokemonIn'=>'10',
+'timeStamp'=>'1405469738'
+}, '&')
+  response = http.request(request)
+}
+case response
+when Net::HTTPSuccess, Net::HTTPRedirection
+parsedJson = JSON.parse(response.entity) #=> {"foo"=>"bar"}
+else
+  response.value
+end
+end
+
+pockemonNo = ["303-0", "304-0", "305-0"]
+
+pockemonNo.each {|pno|
+puts(pno + "のデータを取得します。")
+parsedJson = postPGL('303-0')
+
+nextPokemonId = parsedJson['nextPokemonId']
+rankingPokemonTrend = parsedJson['rankingPokemonTrend']
+
+
+#showHash(parsedJson)
+File.write("hoge.txt", parsedJson)
+
+
+sleepTime = Random.new.rand(1..30)
+puts("取得完了したので、" + sleepTime.to_s + "秒待機します。次は、" + nextPokemonId)
+sleep(sleepTime)
+}
+
+
+
+
+
+client = Mysql2::Client.new(:host => "localhost", :username => "sacchin", :password => "su0u1r0", :database => "pokemon")
+client.query("SELECT id, pokemon_no, time FROM ranking_pokemon_trend").each do |id, pokemon_no, time|
+  p id, pokemon_no, time
+end
+#val1 = '303-0'
+#client.query("INSERT INTO ranking_pokemon_trend (pokemon_no) VALUES ('#{val}')")
+
+
+
+
+
+
+
+
+
+ 
