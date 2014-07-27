8878ff26414ca525a333ff5130d72c757cea643a
satonaka yusuke <saonaka.yusuke@isid.co.jp>
2014-07-28 08:55:44 +0900

    z

--- showtables.rb
+++ showtables.rb
@@ -5,7 +5,7 @@ require 'json'
 require 'mysql2'
 
 client = Mysql2::Client.new(:host => "localhost", :username => "sacchin", :password => "su0u1r0", :database => "pokemon")
-result = client.query("SELECT id, pokemon_no, time FROM ranking_pokemon_trend WHERE pokemon_no = #{pno} ORDER BY time desc")
+result = client.query("SELECT id, pokemon_no, time FROM ranking_pokemon_trend ORDER BY time desc")
 result.each do |row|
 p row
 break
